7381543931268912c25ed0b99595dddfd30fc278
ebazomik <kimosabe.mr@gmail.com>
2026-02-14 00:26:31 +0100

    Update run script with clang and flags

--- run.sh
+++ run.sh
@@ -4,5 +4,13 @@ set -xue
 #QUEMU file path
 QEMU=qemu-system-riscv32
 
+#GCC COMPILER AND FLAGS
+CC=clang
+CFLAGS="-O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
+
+# Build the kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+
 #Start QUEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
